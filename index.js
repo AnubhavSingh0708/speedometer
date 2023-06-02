048a48a0313f3f06a7028430cf74aec589aa6a61
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2023-06-02 07:42:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
 
 
 const options = {
-    enableHighAccuracy: true,
     timeout: 50000,
-    maximumAge: 0,
   };
-  var l0,lt,w0,wt,td,speed;
   function success(pos) {
-    const crd = pos.coords;
-  
-  lt=crd.latitude;
-  wt=crd.longitude
+   var crd = pos.coords.speed;
+    document.getElementById("speed").innerHTML=crd;
+
 
   }
   
   function successl(pos) {
-    const crds = pos.coords;
-  l0=lt;
-  w0=wt;
-  lt=crds.latitude;
-  wt=crds.longitude;
-
-  ld=Math.abs(((lt-l0)/360)*40075);
-  wd=Math.abs(((wt-w0)/360)*40008);
-
-  td=Math.sqrt((Math.pow(ld,2))*(Math.pow(wd,2)));
-  speed=td/720;
-  document.getElementById("speed").innerHTML=speed;
+    var crds = pos.coords.speed;
+  document.getElementById("speed").innerHTML=crds;
   }
 
   function error(err) {
