dfe753b1ca357227607ecc2bd431fc4fc10315fd
Anubhav <68738228+AnubhavSingh0708@users.noreply.github.com>
2023-06-04 15:38:06 +0530

    update

--- meter/index.js
+++ meter/index.js
@@ -15,6 +15,8 @@ wt=pos.coords.longitude;
   function successl(pos) {
     var crds = pos.coords.speed;
   document.getElementById("speed").innerHTML=Math.round(crds);
+  document.getElementById("covers").style.transform = "rotate("+(Math.round(crds)+180.5)+"deg)"
+
 if (crds == null){
   l0=lt;
   w0=wt;
@@ -24,8 +26,9 @@ if (crds == null){
   ld=(l0-lt)*111.3194;
   wd=(w0-wt)*111.133;
 
-  speed=Math.sqrt(Math.pow(ld,2))*Math.pow(ld,2);
+  speed=(Math.sqrt(Math.pow(ld,2))*Math.pow(ld,2))/720;
   document.getElementById("speed").innerHTML=Math.round(speed);
+  document.getElementById("covers").style.transform = "rotate("+(Math.round(speed)+180.5)+"deg)"
 }
   }
 
